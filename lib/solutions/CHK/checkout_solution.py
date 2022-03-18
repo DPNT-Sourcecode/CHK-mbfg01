2ca62adaabf5c6b33b973fe703e114dc9f2986b3
sbx_user1051 <sbx_user1051@169.254.170.149>
2022-03-18 13:02:12 +0000

    Fri Mar 18 13:02:12 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -70,7 +70,7 @@ def checkout(skus):
         "A": [(5, 200), (3, 130)],
         "B": [(2, 45)],
         "H": [(10, 80), (5, 45)],
-        "K": [(2, 150)],
+        "K": [(2, 120)],
         "P": [(5, 200)],
         "Q": [(3, 80)],
         "V": [(3, 130), (2, 90)]
@@ -96,6 +96,7 @@ def checkout(skus):
         # normal price
         res += nb * normal_price[cat]
     return res
+
 
 
 
