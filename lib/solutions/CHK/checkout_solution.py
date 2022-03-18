6baff9b78bb8c754eac999d47a2379d392603b74
sbx_user1051 <sbx_user1051@169.254.170.149>
2022-03-18 11:35:09 +0000

    Fri Mar 18 11:35:09 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,10 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+#  greedy
+
+# input string format? csv? or just like 1A2B3C ??
 def checkout(skus):
-    raise NotImplementedError()
+    #
+
