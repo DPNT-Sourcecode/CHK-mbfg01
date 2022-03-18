1c387464b804b29d03e7e872ad34eb7ff751e375
sbx_user1051 <sbx_user1051@169.254.170.149>
2022-03-18 12:59:12 +0000

    Fri Mar 18 12:59:12 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -24,7 +24,7 @@ def checkout(skus):
         "H": 10,
         "I": 35,
         "J": 60,
-        "K": 80,
+        "K": 70,
         "L": 90,
         "M": 15,
         "N": 40,
@@ -32,14 +32,14 @@ def checkout(skus):
         "P": 50,
         "Q": 30,
         "R": 50,
-        "S": 30,
+        "S": 20,
         "T": 20,
         "U": 40,
         "V": 50,
         "W": 20,
-        "X": 90,
-        "Y": 10,
-        "Z": 50,
+        "X": 17,
+        "Y": 20,
+        "Z": 21,
     }
     # one pass to store
     # we can calculate initial pricing with E and remove free Bs from the dict
@@ -96,5 +96,6 @@ def checkout(skus):
         # normal price
         res += nb * normal_price[cat]
     return res
+
 
 
