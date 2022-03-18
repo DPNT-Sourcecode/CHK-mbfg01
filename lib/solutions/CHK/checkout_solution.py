3212ca8c4a799ae0e68eb4f021f33fa4b954d8fb
sbx_user1051 <sbx_user1051@169.254.170.149>
2022-03-18 11:38:09 +0000

    Fri Mar 18 11:38:09 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,5 +7,15 @@
 
 # input string format? csv? or just like 1A2B3C ??
 def checkout(skus):
-    #
+    mapping = 
+
+    #validate input
+    l = len(skus)
+    if l % 2:
+        return -1
+    for i in range(0, l, 2):
+        if not skus[i].isdigit():
+            return -1
+        nb = 
+
 
