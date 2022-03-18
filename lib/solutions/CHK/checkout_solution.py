a7ed76d9be10f3975c94553a20bebe6dfe01d967
sbx_user1051 <sbx_user1051@169.254.170.149>
2022-03-18 13:11:12 +0000

    Fri Mar 18 13:11:12 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -90,7 +90,10 @@ def checkout(skus):
     
     # for buy any 3 of (S,T,X,Y,Z) for 45, it is also greedy, we will alway have the most expensives singple price ones into the offer, 
     # s t x y z has 20 20 17 20 21, we will get rid of 21 and as many as 20 we can, and we see how we fit the rest 1 or 2 items
-
+    count = 0 
+    for k in group_offer:
+        count += catagory[k]
+    res += 45 * (count // 3)
 
 
     for cat ,nb in catagory.items():
@@ -106,6 +109,7 @@ def checkout(skus):
         # normal price
         res += nb * normal_price[cat]
     return res
+
 
 
 
